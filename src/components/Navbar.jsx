03cae8721e3fca0332cb54dbca4a489dbdd2be07
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2023-03-27 21:32:51 -0500

    cambio de color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-[#242424] flex flex-col justify-center items-center"
         }
       >
         <li className="py-6 text-4xl">
